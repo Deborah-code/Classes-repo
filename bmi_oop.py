be89d47f5bc5d8ef63249d406f8eedfd003cd9e3
Deborah-code <dbrhadewunmi@gmail.com>
2020-10-11 21:32:07 +0100

    hj

--- bmi_oop.py
+++ bmi_oop.py
@@ -1,8 +1,9 @@
+from classes import About_me
 class BMI:
 	def __init__(self, name, weight, height):
-		self.name = name
 		self.weight = weight
 		self.height = height
+		self.name = name
 		
 	def get_name(self):
 		return self.name
@@ -14,16 +15,8 @@ class BMI:
 		return self.height
 
 	def calc_bmi(self, weight, height):
-		return (D.get_name() + " your bmi is " + str(
+		return (D.get_name()+ " your bmi is " + str(
 			(float(weight))/(float(height**2))))
 
 D = BMI('Debbie', 52, 1.6)
-print(D.get_name())
 print(D.calc_bmi(52, 1.6))
-
-class Car(object):
-	def __init__(self, name):
-		self.name = name
-
-user_name = input("What name do you want?")
-first_car = Car(user_name)
